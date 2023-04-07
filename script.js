745a58f78b3886de147998b2ff1fb5481b3917b7
Scott Newman <snewman18@gmail.com>
2023-04-07 10:37:44 -0600

    Auto-adjust the height for the dropped image

--- script.js
+++ script.js
@@ -7,19 +7,13 @@ window.addEventListener('drop', function(evt) {
 }, false);
 
 document.addEventListener('DOMContentLoaded', function() {
-
   document.querySelector('div#content').addEventListener('drop', function(evt) {
     evt.preventDefault();
     evt.stopPropagation();
     unhighlight();
-
-
     let data = evt.dataTransfer;
     let files = data.files;
-
     handleFiles(files);
-
-
   }, false);
 
   document.querySelector('div#content').addEventListener('dragenter', highlight, false);
@@ -30,10 +24,14 @@ document.addEventListener('DOMContentLoaded', function() {
 function handleFiles(files) {
 	let filename = URL.createObjectURL(files[0]);
 	let imageElement = document.createElement('img');
+  imageElement.setAttribute('id', 'screenshot');
 	imageElement.setAttribute('src', filename);
-
   document.querySelector('div#content').innerHTML = '';
 	document.querySelector('div#content').appendChild(imageElement);
+
+  setTimeout(function() {
+    resizeBrowser();
+  }, 500);
 }
 
 function highlight() {
@@ -42,4 +40,11 @@ function highlight() {
 
 function unhighlight() {
   document.querySelector('div#content').classList.remove('highlight');
+}
+
+function resizeBrowser() {
+  let viewportElement = document.querySelector('section#device-viewport');
+  let imgHeight = document.querySelector('img#screenshot').height;
+  let headerHeight = 70;
+  viewportElement.style.height = imgHeight + headerHeight + 'px';
 }
